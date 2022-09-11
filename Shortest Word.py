dcc845ecb6f665875de6c03c11b84787c4d64294
Maria Aguilà <boo143256@gmail.com>
2022-09-11 11:54:37 +0200

    training

--- Shortest Word.py
+++ Shortest Word.py
@@ -1,10 +1,14 @@
 def find_short(s):
-    counti = 0
-    while(s == ' '):
-        counti += 1
-    print(counti)
+    x = s.split(" ")
+    lenght = []
+    for i in x:
+        lenght.append(len(i))
+    lenght.sort()
+    return lenght[0]
     #return l # l: shortest word length
 
 
 text= "bitcoin take over the world maybe who knows perhaps"
+text= "turns out random test cases are easier than writing out basic ones"
+text= "i want to travel the world writing code one day"
 print(find_short(text))
